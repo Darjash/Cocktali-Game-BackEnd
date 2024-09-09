0e6578be3bb97318ed9148e120cb96278759fa9e
Darjash <darja.skiljova@gmail.com>
2024-09-09 12:13:58 +0300

    add DisplayName to tests and improve letter revealing tests

--- src/test/java/com/ridango/GameServiceTest.java
+++ src/test/java/com/ridango/GameServiceTest.java
@@ -2,13 +2,16 @@ package com.ridango;
 
 import com.ridango.game.model.Cocktail;
 import com.ridango.game.model.GameSession;
+import com.ridango.game.repository.HighScoreRepo;
 import com.ridango.game.service.CocktailService;
 import com.ridango.game.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.util.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -16,6 +19,9 @@ import static org.mockito.Mockito.when;
 public class GameServiceTest {
     @Mock
     private CocktailService cocktailService;
+
+    @Mock
+    private HighScoreRepo highScoreRepo;
 
     @InjectMocks
     private GameService gameService;
@@ -30,6 +36,7 @@ public class GameServiceTest {
     }
 
     @Test
+    @DisplayName("Should start a new game session with 5 attempts and a hidden cocktail name")
     void testStartNewGame() {
         GameSession session = gameService.startNewGame();
 
@@ -39,6 +46,7 @@ public class GameServiceTest {
     }
 
     @Test
+    @DisplayName("Should correctly guess the cocktail and start a new game with a full score")
     void testMakeCorrectGuess(){
         gameService.startNewGame();
         GameSession session = gameService.makeGuess("Mojito");
@@ -49,15 +57,39 @@ public class GameServiceTest {
     }
 
     @Test
+    @DisplayName("Should reduce attempts left by 1 after an incorrect guess and reveal one letter")
     void testMakeIncorrectGuess() {
         gameService.startNewGame();
         GameSession session = gameService.makeGuess("WrongGuess");
+        String hiddenName = session.getHiddenCocktailName();
+        int countHiddenLetters = StringUtils.countOccurrencesOf(hiddenName, "_");
 
         assertNotNull(session);
         assertEquals(4, session.getAttemptsLeft());
+        assertEquals(5, countHiddenLetters);
     }
 
     @Test
+    @DisplayName("Should reduce attempts left by 1 after an incorrect guess and reveal two letters")
+    void testMakeIncorrectGuessOnLongCocktail() {
+        Cocktail longCocktail = new Cocktail();
+        longCocktail.setStrDrink("Banana Cantaloupe Smoothie");
+        when(cocktailService.fetchRandomCocktail()).thenReturn(longCocktail);
+
+        gameService.startNewGame();
+
+        GameSession session = gameService.makeGuess("WrongGuess");
+
+        String hiddenName = session.getHiddenCocktailName();
+        int countHiddenLetters = StringUtils.countOccurrencesOf(hiddenName, "_");
+
+        assertNotNull(session);
+        assertEquals(4, session.getAttemptsLeft());
+        assertEquals(22, countHiddenLetters);
+    }
+
+    @Test
+    @DisplayName("Should restart the game and reset attempts to 5 and score to 0")
     void testRestartGame() {
         GameSession session = gameService.restartGame();
 
--- src/test/java/com/ridango/HighScoreRepoTest.java
+++ src/test/java/com/ridango/HighScoreRepoTest.java
@@ -3,6 +3,7 @@ package com.ridango;
 import com.ridango.game.CocktailGameApplication;
 import com.ridango.game.model.HighScore;
 import com.ridango.game.repository.HighScoreRepo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,7 @@ public class HighScoreRepoTest {
     private HighScoreRepo highScoreRepo;
 
     @Test
+    @DisplayName("Should save a high score and assign an ID")
     void testSaveHighScore() {
         HighScore highScore = new HighScore(100);
 
@@ -28,6 +30,7 @@ public class HighScoreRepoTest {
     }
 
     @Test
+    @DisplayName("Should find the highest score from the repository")
     void testFindHighestScore() {
         highScoreRepo.save(new HighScore(200));
         highScoreRepo.save(new HighScore(100));
